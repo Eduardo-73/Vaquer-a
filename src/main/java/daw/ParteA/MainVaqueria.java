322baee104fdbc94d144ca580b91e43f0aa228c2
Eduardo <eduardocorreaaranda10@gmail.com>
2024-01-31 20:14:10 +0100

    Terminado

--- src/main/java/daw/ParteA/MainVaqueria.java
+++ src/main/java/daw/ParteA/MainVaqueria.java
@@ -17,11 +17,33 @@ public class MainVaqueria {
     public static void main(String[] args) {
 
         Vaqueria vaca = new Vaqueria();
+        Vaqueria vaca2 = new Vaqueria();
         Vaca v1 = new Vaca("1", LocalDate.now(), "Pepe");
         Vaca v2 = new Vaca("2", LocalDate.now(), "Pepa");
         Vaca v3 = new Vaca("3", LocalDate.now(), "Andrés");
         Vaca v4 = new Vaca("4", LocalDate.now(), "Andrea");
         Vaca v5 = new Vaca("5", LocalDate.now(), "David");
+        Vaca v6 = new Vaca("5", LocalDate.now(), "David");
 
+        System.out.println(vaca.numeroVacas());
+        vaca.aniadirVaca(v1);
+        vaca.aniadirVaca(v2);
+        vaca.aniadirVaca(v3);
+        vaca.aniadirVaca(v4);
+        vaca.aniadirVaca(v5);
+
+        System.out.println(vaca.numeroVacas());
+
+        System.out.println(vaca.hayVacas());
+        System.out.println(vaca2.hayVacas());
+
+        System.out.println(vaca.animalEnVaqueria(v5));
+        System.out.println(vaca.animalEnVaqueria(v6));
+
+        vaca.darBajaVaca(v3);
+
+        for (Vaca vacas : vaca.sacarAnimalesArrayList()) {
+            System.out.println(vacas);
+        }
     }
 }
--- src/main/java/daw/ParteB/MainVaqueriaOrdenada.java
+++ src/main/java/daw/ParteB/MainVaqueriaOrdenada.java
@@ -4,6 +4,10 @@
  */
 package daw.ParteB;
 
+import daw.ParteA.Vaqueria;
+import daw.Vaca;
+import java.time.LocalDate;
+
 /**
  *
  * @author edu
@@ -12,5 +16,34 @@ public class MainVaqueriaOrdenada {
 
     public static void main(String[] args) {
 
+        VaqueriaOrdenada vo = new VaqueriaOrdenada();
+        VaqueriaOrdenada vaca2 = new VaqueriaOrdenada();
+        Vaca v1 = new Vaca("1", LocalDate.now(), "Pepe");
+        Vaca v2 = new Vaca("2", LocalDate.now(), "Pepa");
+        Vaca v3 = new Vaca("3", LocalDate.now(), "Andrés");
+        Vaca v4 = new Vaca("4", LocalDate.now(), "Andrea");
+        Vaca v5 = new Vaca("5", LocalDate.now(), "David");
+        Vaca v6 = new Vaca("5", LocalDate.now(), "David");
+
+        System.out.println(vo.numeroVacas());
+        vo.aniadirVaca(v1);
+        vo.aniadirVaca(v2);
+        vo.aniadirVaca(v3);
+        vo.aniadirVaca(v4);
+        vo.aniadirVaca(v5);
+
+        System.out.println(vo.numeroVacas());
+
+        System.out.println(vo.hayVacas());
+        System.out.println(vaca2.hayVacas());
+
+        System.out.println(vo.animalEnVaqueria(v5));
+        System.out.println(vo.animalEnVaqueria(v6));
+
+        vo.darBajaVaca(v3);
+
+        for (Vaca vacas : vo.sacarAnimalesArrayList()) {
+            System.out.println(vacas);
+        }
     }
 }
